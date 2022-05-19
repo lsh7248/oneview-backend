70cab2649016da9111ec18641f8ee3d333a50969
10151032 <s-hyun.lee@kt.com>
2022-05-19 16:11:12 +0900

    main.py "/" api 작성.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,35 @@
-from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+def create_app():
+    """
+    앱 함수 실행
+    :return:
+    """
+    app = FastAPI()
+    # 데이터 베이스 이니셜라이즈
+
+    # 레디스 이니셜라이즈
+
+    # 미들웨어 정의
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    # 라우터 정의
+
+    return app
 
 
+app = create_app()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
