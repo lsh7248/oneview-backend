9bd634cae49485a4eae73e4af02891d4eeafec95
10151032 <s-hyun.lee@kt.com>
2022-05-23 09:39:51 +0900

    endpoint user.py 수정

--- app/routers/api/api_v1/endpoints/users.py
+++ app/routers/api/api_v1/endpoints/users.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.crud.user import create_user, get_users, get_user, get_user_by_employee_id
-from app.dependency import get_db
+from app.routers.api.deps import get_db
 from app.schemas.user import User, UserCreate
 
 router = APIRouter()
@@ -14,7 +14,7 @@ router = APIRouter()
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_employee_id(db, employee_id=user.employee_id)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=400, detail="User already registered")
     return create_user(db=db, user=user)
 
 
@@ -27,6 +27,13 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 @router.get("/{id}", response_model=User)
 def read_user(id: int, db: Session = Depends(get_db)):
     db_user = get_user(db, id=id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@router.get("/emp/{id}", response_model=User)
+def read_user_by_empid(id: int, db: Session = Depends(get_db)):
+    db_user = get_user_by_employee_id(db, id=id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
